a5cecc027851a6529a364ab00508b091f8f4d084
rananomi05 <noman987rajpoot@gmail.com>
2026-05-31 16:33:39 +0500

    fix nextauth session type error

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,21 +1,21 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  interface User {
-    role?: string;
-  }
-
   interface Session {
     user: {
-      role?: string;
-      id?: string;
+      id: string;
+      role: string;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    role?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    role?: string;
-    id?: string;
+    id: string;
+    role: string;
   }
 }
